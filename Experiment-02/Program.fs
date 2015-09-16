112cafcbc99c680bbd8d58fccce416add0ccd6bd
Jim Killingsworth <jkillingsworth@gmail.com>
2015-09-15 22:57:44 -0400

    Update chart output path

--- Experiment-02/Program.fs
+++ Experiment-02/Program.fs
@@ -8,7 +8,7 @@ let random = Random()
 let policy = Compute.AlwaysTakeHi
 let result = Compute.generateResults random policy |> Seq.take (10 + 1) |> Seq.toArray
 
-Chart.renderChart (sprintf @"..\..\..\02-%A.png" policy) result
+Chart.renderChart @"..\..\..\Experiment-02.png" result
 
 for (value, bid, ask) in result do
     printfn "(%f, %f) %f" bid ask (ask - bid)

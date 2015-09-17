db4436612362575429f2a83ac4e215d78a90906a
Jim Killingsworth <jkillingsworth@gmail.com>
2015-09-16 22:39:22 -0400

    Add experiment 04

---
+++ Experiment-04/Chart.fs
@@ -0,0 +1,85 @@
+﻿module Chart
+
+open OxyPlot
+open OxyPlot.Axes
+open OxyPlot.Series
+
+//-------------------------------------------------------------------------------------------------
+
+let private exportToPng path w h model =
+
+    use writeStream = System.IO.File.OpenWrite(path)
+    let pngExporter = OxyPlot.WindowsForms.PngExporter()
+    pngExporter.Width <- w
+    pngExporter.Height <- h
+    pngExporter.Export(model, writeStream)
+
+let private defaultColorsToUseForPlots =
+
+    [| OxyColors.Red
+       OxyColors.Blue
+       OxyColors.Green
+       OxyColors.Orange
+       OxyColors.Purple
+       OxyColors.Teal |]
+
+//-------------------------------------------------------------------------------------------------
+
+let renderChart path data =
+
+    let model = PlotModel()
+
+    model.DefaultColors <- defaultColorsToUseForPlots
+    model.LegendBackground <- OxyColors.White
+    model.LegendBorder <- OxyColors.Gray
+    model.LegendBorderThickness <- 1.0
+    model.LegendPlacement <- LegendPlacement.Inside
+    model.LegendPosition <- LegendPosition.RightTop
+    model.PlotMargins <- OxyThickness(nan, nan, 10.0, nan)
+
+    let axis = LinearAxis()
+    axis.Title <- "Time"
+    axis.Position <- AxisPosition.Bottom
+    axis.Minimum <- 0.0
+    axis.Maximum <- float (Array.length data - 1)
+    axis.MajorStep <- 1.0
+    axis.MinorStep <- 1.0
+    axis.MajorGridlineColor <- OxyColors.LightGray
+    axis.MajorGridlineStyle <- LineStyle.Dot
+    model.Axes.Add(axis)
+
+    let axis = LinearAxis()
+    axis.Title <- "Price"
+    axis.Position <- AxisPosition.Left
+    axis.Minimum <- Compute.valueLower - 0.05
+    axis.Maximum <- Compute.valueUpper + 0.05
+    axis.MajorStep <- 0.25
+    axis.MinorStep <- 0.05
+    axis.MajorGridlineColor <- OxyColors.LightGray
+    axis.MajorGridlineStyle <- LineStyle.Dot
+    axis.MinorGridlineColor <- OxyColors.LightGray
+    axis.MinorGridlineStyle <- LineStyle.Dot
+    axis.StringFormat <- "F2"
+    axis.AxisTitleDistance <- 9.0
+    model.Axes.Add(axis)
+
+    let series = LineSeries()
+    series.Title <- "True price"
+    series.StrokeThickness <- 1.0
+    data
+    |> Array.mapi (fun i (value, bid, ask) -> DataPoint(float i, value))
+    |> Array.iter series.Points.Add
+    model.Series.Add(series)
+
+    let series = AreaSeries()
+    series.Title <- "MM price"
+    series.StrokeThickness <- 1.0
+    data
+    |> Array.mapi (fun i (value, bid, ask) -> DataPoint(float i, bid))
+    |> Array.iter series.Points.Add
+    data
+    |> Array.mapi (fun i (value, bid, ask) -> DataPoint(float i, ask))
+    |> Array.iter series.Points2.Add
+    model.Series.Add(series)
+
+    model |> exportToPng path 700 400
---
+++ Experiment-04/Compute.fs
@@ -0,0 +1,111 @@
+﻿module Compute
+
+open MathNet.Numerics.Distributions
+open MathNet.Numerics.Integration
+
+//-------------------------------------------------------------------------------------------------
+
+let valueUpper = 51.0
+let valueLower = 50.0
+
+let probInit value = ContinuousUniform.PDF(valueLower, valueUpper, value)
+
+let probInf = 0.5
+let probUnf = 1.0 - probInf
+
+let probInfTake value = ContinuousUniform.CDF(valueLower, valueUpper, value)
+let probInfSell value = 1.0 - probInfTake value
+
+let probUnfTake value = 0.5
+let probUnfSell value = 0.5
+
+let probTake value = (probInf * probInfTake value) + (probUnf * probUnfTake value)
+let probSell value = (probInf * probInfSell value) + (probUnf * probUnfSell value)
+
+//-------------------------------------------------------------------------------------------------
+
+let private integrate f lower upper =
+    let f = System.Func<_,_>(f)
+    let partitions = 1000
+    NewtonCotesTrapeziumRule.IntegrateComposite(f, valueLower, valueUpper, partitions)
+
+let private computeBid p =
+
+    let f value = (p value) * (probSell value)
+    let probSellOverall = integrate f valueLower valueUpper
+    let probValueGivenSell value = (f value) / probSellOverall
+
+    let f value = value * (probValueGivenSell value)
+    integrate f valueLower valueUpper
+
+let private computeAsk p =
+
+    let f value = (p value) * (probTake value)
+    let probTakeOverall = integrate f valueLower valueUpper
+    let probValueGivenTake value = (f value) / probTakeOverall
+
+    let f value = value * (probValueGivenTake value)
+    integrate f valueLower valueUpper
+
+let private executeTake p =
+
+    let p' value = (p value) * (probTake value)
+
+    let bid = computeBid p'
+    let ask = computeAsk p'
+
+    (bid, ask, p')
+
+let private executeSell p =
+
+    let p' value = (p value) * (probSell value)
+
+    let bid = computeBid p'
+    let ask = computeAsk p'
+
+    (bid, ask, p')
+
+let private executeNone p =
+
+    let p' = p
+
+    let bid = computeBid p'
+    let ask = computeAsk p'
+
+    (bid, ask, p')
+
+//-------------------------------------------------------------------------------------------------
+
+type ExecutionPolicy =
+    | Stochastic of float
+
+let private getValue = function
+    | Stochastic value -> value
+
+let private getExecuteFunc random (value, bid, ask) =
+    match Sample.continuousUniform 0.0 1.0 random with
+    | sample when (sample < probInf) && (value > ask) -> executeTake
+    | sample when (sample < probInf) && (value < bid) -> executeSell
+    | sample when (sample < probInf) -> executeNone
+    | sample when (sample < probInf + probUnf * 0.50)
+        -> executeTake
+    | _ -> executeSell
+
+let generateResults random executionPolicy =
+
+    let p = probInit
+
+    let bid = computeBid p
+    let ask = computeAsk p
+
+    let value = getValue executionPolicy
+
+    let generator (bid, ask, p) =
+        let execute = getExecuteFunc random (value, bid, ask)
+        let (bid, ask, p) = execute p
+        Some ((value, bid, ask), (bid, ask, p))
+
+    seq {
+        yield (value, bid, ask)
+        yield! Seq.unfold generator (bid, ask, p)
+    }

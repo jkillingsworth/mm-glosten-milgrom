1ce8db634b2e08402e9d4ac97beac47ded39beaa
Jim Killingsworth <jkillingsworth@gmail.com>
2015-11-01 13:59:03 -0500

    Add experiment 06

---
+++ Experiment-06/Program.fs
@@ -0,0 +1,17 @@
+﻿module Program
+
+open System
+
+//-------------------------------------------------------------------------------------------------
+
+let random = Random()
+let policy = Compute.Stochastic 50.75
+let result = Compute.generateResults random policy |> Seq.take (25 + 1) |> Seq.toArray
+
+Chart.renderPrices @"..\..\..\Experiment-06-Prices.png" result
+Chart.renderBelief @"..\..\..\Experiment-06-Belief.png" result.[25]
+
+for (value, bid, ask, p) in result do
+    let _, _, vBid = bid
+    let _, _, vAsk = ask
+    printfn "(%f, %f) %f" vBid vAsk (vAsk - vBid)
